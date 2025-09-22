b66307d524ee66ccc3aba7b4ee3c0bc3ccb8d99d
oxydaid <aghatabns5@gmail.com>
2025-09-22 10:14:52 +0700

    update terus

--- src/TokopayServiceProvider.php
+++ src/TokopayServiceProvider.php
@@ -3,23 +3,22 @@
 namespace Oxydaid\Tokopay;
 
 use Illuminate\Support\ServiceProvider;
-use Oxydaid\Tokopay\Tokopay as TokopayService;
 
 class TokopayServiceProvider extends ServiceProvider
 {
     public function register()
     {
         $this->app->singleton('tokopay', function () {
-            return new TokopayService();
+            return new Tokopay();
         });
 
-        $this->mergeConfigFrom(__DIR__ . '/config/tokopay.php', 'tokopay');
+        $this->mergeConfigFrom(__DIR__ . '/../config/tokopay.php', 'tokopay');
     }
 
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '/config/tokopay.php' => config_path('tokopay.php'),
+            __DIR__ . '/../config/tokopay.php' => config_path('tokopay.php'),
         ], 'config');
     }
 }
